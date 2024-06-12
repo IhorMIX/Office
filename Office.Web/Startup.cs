1e3a04487c758e02b209cd3884fad82f7be9de93
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-06-12 22:02:46 +0300

    update

---
+++ Office.Web/Startup.cs
@@ -0,0 +1,28 @@
+namespace Office.Web;
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+    }
+}
