1fb73008165e6ba7650a87a4423bf5e3a4ff9b2b
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-12-18 00:10:49 +0200

    Revert "check reset"
    This reverts commit 935ac3e6a2c29603a33daa4da93e25baaa1f92da.

--- Office.BLL/Services/Interfaces/IEmployeeService.cs
+++ Office.BLL/Services/Interfaces/IEmployeeService.cs
@@ -7,6 +7,7 @@ public interface IEmployeeService : IBasicService<EmployeeModel>
     Task<EmployeeModel> CreateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default);
     Task<EmployeeModel> UpdateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default);
     Task DeleteEmployeeAsync(int id, CancellationToken cancellationToken = default);
+    
     Task<List<EmployeeModel>> GetEmployeesAsync(int managerId, CancellationToken cancellationToken = default);
     Task<List<EmployeeModel>> GetAllAsync(int managerId, CancellationToken cancellationToken = default);
 }
