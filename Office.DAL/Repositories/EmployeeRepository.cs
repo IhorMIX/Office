fc220b55dbd23b1603a0be29dfc7fe60b7a64980
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-02-21 00:38:20 +0200

    repository

--- Office.DAL/Repositories/EmployeeRepository.cs
+++ Office.DAL/Repositories/EmployeeRepository.cs
@@ -13,6 +13,7 @@ public class EmployeeRepository(OfficeDbContext officeDbContext) : IEmployeeRepo
     {
         return _officeDbContext.BaseEmployees
             .Include(i=>((Employee)i).Position)
+            .Include(r => ((Employee)r).Subdivision)
             .AsQueryable();
     }
 
@@ -25,6 +26,9 @@ public class EmployeeRepository(OfficeDbContext officeDbContext) : IEmployeeRepo
     public IQueryable<Employee> GetAllEmployees()
     {
         return _officeDbContext.Employees
+            .Include(r => r.Position)
+            .Include(r => r.Subdivision)
+            .Include(r => r.HrManager)
             .AsQueryable();
     }
     public IQueryable<HrManager> GetAllHrManagers()
@@ -40,9 +44,17 @@ public class EmployeeRepository(OfficeDbContext officeDbContext) : IEmployeeRepo
             .AsQueryable();
     }
     
-    public Task<BaseEmployee?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    public async Task<BaseEmployee?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        return _officeDbContext.BaseEmployees.SingleOrDefaultAsync(i => i.Id == id, cancellationToken: cancellationToken);
+        return await _officeDbContext.BaseEmployees
+            .Include(r => ((Employee)r).Subdivision)
+            .Include(r => ((Employee)r).Position)
+            .Include(r => ((Employee)r).Projects)
+            .Include(r => ((Employee)r).LeaveRequests)
+            .Include(r => ((Employee)r).HrManager)
+            .Include(r => ((HrManager)r).Workers)
+            .Include(r => ((ProjectManager)r).Projects).ThenInclude(r => r.ProjectType)
+            .SingleOrDefaultAsync(r => r.Id == id, cancellationToken);
     }
 
     public async Task<BaseEmployee> AddEmployeeAsync(BaseEmployee employee, CancellationToken cancellationToken = default)
