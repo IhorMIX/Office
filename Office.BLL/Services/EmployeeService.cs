f21bf46d405c167cc8a3aabbd0f58d86634b9198
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-02-21 00:38:48 +0200

    added auth service and fix employee

--- Office.BLL/Services/EmployeeService.cs
+++ Office.BLL/Services/EmployeeService.cs
@@ -1,8 +1,11 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Office.BLL.Exceptions;
+using Office.BLL.Helpers;
 using Office.BLL.Models;
 using Office.BLL.Services.Interfaces;
+using Office.DAL.Entity;
+using Office.DAL.Entity.Employees;
 using Office.DAL.Repositories.Intefaces;
 
 
@@ -16,6 +19,8 @@ public class EmployeeService(IEmployeeRepository employeeRepository, IMapper map
     public async Task<EmployeeModel> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
         var employeeDb = await _employeeRepository.GetAllEmployees()
+            .Include(e => e.Projects)
+            .ThenInclude(p => p.ProjectManager)
             .SingleOrDefaultAsync(i => i.Id == id, cancellationToken);
         
         if (employeeDb is null)
@@ -24,9 +29,24 @@ public class EmployeeService(IEmployeeRepository employeeRepository, IMapper map
         return _mapper.Map<EmployeeModel>(employeeDb);
     }
 
-    public Task<EmployeeModel> CreateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default)
+    public async Task<EmployeeModel> CreateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var creator = await _employeeRepository.GetAll().Where(r => r.Id == managerId && (r is HrManager || r is Admin)).SingleOrDefaultAsync(cancellationToken);
+        if (creator is null)
+            throw new EmployeeNotFoundException($"Manager with Id {managerId} not found");
+        
+        var employeeDb = await _employeeRepository.GetAll().FirstOrDefaultAsync(i => i.Login == employeeModel.Login, cancellationToken);
+        if (employeeDb is not null)
+        {
+            if (employeeDb.Login == employeeModel.Login)
+                throw new AlreadyLoginException("Login is already used by another employee");
+        }
+        
+        employeeModel.Password = PasswordHelper.HashPassword(employeeModel.Password);
+        if (creator is HrManager)
+            employeeModel.HrManagerId = creator.Id;
+        employeeDb = await _employeeRepository.AddEmployeeAsync(_mapper.Map<Employee>(employeeModel), cancellationToken);
+        return _mapper.Map<EmployeeModel>(await _employeeRepository.GetByIdAsync(employeeDb.Id, cancellationToken));
     }
 
     public Task<EmployeeModel> UpdateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default)
@@ -34,9 +54,14 @@ public class EmployeeService(IEmployeeRepository employeeRepository, IMapper map
         throw new NotImplementedException();
     }
 
-    public Task DeleteEmployeeAsync(int id, CancellationToken cancellationToken = default)
+    public async Task DeleteEmployeeAsync(int id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var employeeDb = await _employeeRepository.GetByIdAsync(id, cancellationToken);
+        
+        if (employeeDb is null)
+            throw new EmployeeNotFoundException($"Employee with Id {id} not found");
+
+        await _employeeRepository.DeleteEmployeeAsync(employeeDb, cancellationToken);
     }
 
     public Task<List<EmployeeModel>> GetEmployeesAsync(int managerId, CancellationToken cancellationToken = default)
--- Office.BLL/Services/Interfaces/IEmployeeService.cs
+++ Office.BLL/Services/Interfaces/IEmployeeService.cs
@@ -7,7 +7,6 @@ public interface IEmployeeService : IBasicService<EmployeeModel>
     Task<EmployeeModel> CreateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default);
     Task<EmployeeModel> UpdateEmployeeAsync(int managerId, EmployeeModel employeeModel, CancellationToken cancellationToken = default);
     Task DeleteEmployeeAsync(int id, CancellationToken cancellationToken = default);
-    
     Task<List<EmployeeModel>> GetEmployeesAsync(int managerId, CancellationToken cancellationToken = default);
     Task<List<EmployeeModel>> GetAllAsync(int managerId, CancellationToken cancellationToken = default);
 }
