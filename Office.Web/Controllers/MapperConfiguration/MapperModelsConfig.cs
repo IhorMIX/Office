bd9ed9ed9cc6d07915443df303c021f2564c2c98
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-02-21 18:51:18 +0200

    sorted mapping and added exception

--- Office.Web/Controllers/MapperConfiguration/MapperModelsConfig.cs
+++ Office.Web/Controllers/MapperConfiguration/MapperModelsConfig.cs
@@ -10,36 +10,28 @@ public class MapperModelsConfig : AutoMapper.Profile
 {
     public MapperModelsConfig()
     {
-        CreateMap<BaseEmployee, BaseEmployeeModel>()
-            .ReverseMap();
+        CreateMap<BaseEmployee, BaseEmployeeModel>().ReverseMap();
+        CreateMap<Employee, EmployeeModel>().ReverseMap();
+        CreateMap<Employee, BaseEmployeeModel>().ReverseMap();
+        CreateMap<EmployeeModel, BaseEmployeeModel>().ReverseMap();
+        CreateMap<BaseManager, BaseManagerModel>().ReverseMap();
+        CreateMap<HrManager, HrManagerModel>().ReverseMap();
+        CreateMap<ProjectManager, ProjectManagerModel>().ReverseMap();
         
-        CreateMap<EmployeeModel, BaseEmployeeModel>()
-            .ReverseMap();
         CreateMap<AuthorizationInfoModel, AuthorizationInfo>()
-            .ForMember(m => m.EmployeeId, o => o.Ignore())
-            .ForMember(m => m.Employee, o => o.Ignore())
+            .ForMember(dest => dest.EmployeeId, opt => opt.Ignore())
+            .ForMember(dest => dest.Employee, opt => opt.Ignore())
             .ReverseMap();
         
-        CreateMap<BaseManager, BaseManagerModel>()
-            .ReverseMap();
-        
-        CreateMap<Employee, EmployeeModel>()
-            .ReverseMap();
-        CreateMap<Employee, BaseEmployeeModel>()
-            .ReverseMap();
-        CreateMap<HrManager, HrManagerModel>()
-            .ReverseMap();
-        
-        CreateMap<ProjectManager, ProjectManagerModel>()
-            .ReverseMap();
         CreateMap<Position, PositionModel>().ReverseMap();
         CreateMap<Subdivision, SubdivisionModel>().ReverseMap();
-        CreateMap<EmployeeViewModel, EmployeeModel>()
-            .ReverseMap();
+        
+        CreateMap<EmployeeViewModel, EmployeeModel>().ReverseMap();
         CreateMap<EmployeeCreateModel, EmployeeModel>().ReverseMap();
+        
         CreateMap<SubdivisionModel, SelectionViewModel>().ReverseMap();
         CreateMap<PositionModel, SelectionViewModel>().ReverseMap();
-
+        
         CreateMap<BaseEmployeeModel, CurrentUserViewModel>()
             .ForMember(dest => dest.EmployeeType, opt => opt.Ignore())
             .ReverseMap();
