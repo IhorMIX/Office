f2872ffec8c0cd9ab439cae3b687253388d4335b
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-03-31 22:17:26 +0300

    projectType's repository

---
+++ Office.DAL/Repositories/Intefaces/IProjectTypeRepository.cs
@@ -0,0 +1,11 @@
+using Office.DAL.Entity.Selections;
+
+namespace Office.DAL.Repositories.Intefaces;
+
+public interface IProjectTypeRepository : IBasicRepository<ProjectType>
+{
+    Task<ProjectType> CreateProjectTypeAsync(ProjectType projectType, CancellationToken cancellationToken = default);
+    Task DeleteProjectTypeAsync(ProjectType projectType, CancellationToken cancellationToken = default);
+    
+    Task UpdateProjectTypeAsync(ProjectType projectType, CancellationToken cancellationToken = default);
+}
---
+++ Office.DAL/Repositories/ProjectTypeRepository.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Office.DAL.Entity.Selections;
+using Office.DAL.Repositories.Intefaces;
+
+namespace Office.DAL.Repositories;
+
+public class ProjectTypeRepository(OfficeDbContext officeDbContext) : IProjectTypeRepository
+{
+    public IQueryable<ProjectType> GetAll()
+    {
+        return officeDbContext.ProjectTypes.AsQueryable();
+    }
+
+    public async Task<ProjectType?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await officeDbContext.ProjectTypes.SingleOrDefaultAsync(r => r.Id == id, cancellationToken);
+    }
+
+    public async Task<ProjectType> CreateProjectTypeAsync(ProjectType projectType, CancellationToken cancellationToken = default)
+    {
+        var entity = await officeDbContext.ProjectTypes.AddAsync(projectType,cancellationToken);
+        await officeDbContext.SaveChangesAsync(cancellationToken);
+        return entity.Entity;
+    }
+
+    public async Task DeleteProjectTypeAsync(ProjectType projectType, CancellationToken cancellationToken = default)
+    {
+        officeDbContext.ProjectTypes.Remove(projectType);
+        await officeDbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateProjectTypeAsync(ProjectType projectType, CancellationToken cancellationToken = default)
+    {
+        officeDbContext.ProjectTypes.Update(projectType);
+        await officeDbContext.SaveChangesAsync(cancellationToken);
+    }
+}
