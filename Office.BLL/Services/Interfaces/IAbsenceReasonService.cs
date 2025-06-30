1f2211422c25f6001be2eae9099b362cdb4a3a7e
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-06-30 15:37:03 +0300

    renamed filed

--- Office.BLL/Services/Interfaces/IAbsenceReasonService.cs
+++ Office.BLL/Services/Interfaces/IAbsenceReasonService.cs
@@ -5,7 +5,7 @@ namespace Office.BLL.Services.Interfaces;
 
 public interface IAbsenceReasonService: IBasicService<AbsenceReasonModel>
 {
-    Task<AbsenceReason> CreateAbsenceReasonAsync(string absenseDesc,int managerId, CancellationToken cancellationToken = default);
+    Task<AbsenceReason> CreateAbsenceReasonAsync(string description,int managerId, CancellationToken cancellationToken = default);
     Task DeleteAbsenceReasonAsync(int positionId,int managerId, CancellationToken cancellationToken = default);
     Task UpdateAbsenceReasonAsync(AbsenceReasonModel position, CancellationToken cancellationToken = default);
 }
