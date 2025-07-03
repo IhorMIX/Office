076a7c18438c7d125943acd5b96febd18cb632de
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-07-03 13:46:22 +0300

    position update endpoint

--- Office.BLL/Services/Interfaces/IPositionService.cs
+++ Office.BLL/Services/Interfaces/IPositionService.cs
@@ -1,4 +1,5 @@
 using Office.BLL.Models;
+using Office.DAL.Entity.Selections;
 
 namespace Office.BLL.Services.Interfaces;
 
@@ -6,5 +7,6 @@ public interface IPositionService: IBasicService<PositionModel>
 {
     Task<PositionModel> CreatePositionAsync(PositionModel position,int managerId, CancellationToken cancellationToken = default);
     Task DeletePositionAsync(int positionId, int managerId, CancellationToken cancellationToken = default);
-    Task UpdatePositionAsync(PositionModel position, CancellationToken cancellationToken = default);
+    Task UpdatePositionAsync(int managerId, Position position,
+        CancellationToken cancellationToken = default);
 }
