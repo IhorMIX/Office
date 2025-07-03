f5d1474d4d068b792aa5afc072dd02269900d113
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-07-03 13:58:31 +0300

    project update endpoint

--- Office.BLL/Services/ProjectService.cs
+++ Office.BLL/Services/ProjectService.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Office.BLL.Exceptions;
+using Office.BLL.Helpers;
 using Office.BLL.Models;
 using Office.BLL.Services.Interfaces;
 using Office.DAL.Entity;
@@ -45,9 +46,49 @@ public class ProjectService(IProjectRepository projectRepository, IMapper mapper
         await projectRepository.DeleteProjectAsync(projectDb, cancellationToken);
     }
 
-    public Task<ProjectModel> UpdateProjectAsync(int projectManagerId, ProjectModel projectModel, CancellationToken cancellationToken = default)
+    public async Task<ProjectModel> UpdateProjectAsync(int projectManagerId, ProjectModel projectModel,
+        CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var managerDb = await employeeRepository.GetAll()
+            .SingleOrDefaultAsync(r => r.Id == projectManagerId && (r is ProjectManager || r is Admin),
+                cancellationToken);
+        if (managerDb is null)
+            throw new ManagerException($"Project manager or admin with Id {projectManagerId} not found");
+
+        var updateProjectManager = await employeeRepository.GetAll()
+            .SingleOrDefaultAsync(r => r.Id == projectModel.ProjectManagerId && (r is ProjectManager || r is Admin),
+                cancellationToken);
+        if (updateProjectManager is null)
+            throw new ManagerException(
+                $"Project manager or admin with Id {projectModel.ProjectManagerId} not found");
+
+        var projectDb = await projectRepository.GetByIdAsync(projectModel.Id, cancellationToken);
+        if (projectDb is null)
+            throw new EntityNotFoundException($"Project with Id {projectModel.Id} not found");
+
+        if (managerDb is Admin || managerDb is ProjectManager && projectDb.ProjectManagerId == managerDb.Id)
+        {
+            foreach (var propertyMap in ReflectionHelper.WidgetUtil<ProjectModel, Project>.PropertyMap)
+            {
+                var userProperty = propertyMap.Item1;
+                var userDbProperty = propertyMap.Item2;
+
+                var userSourceValue = userProperty.GetValue(projectModel);
+                var userTargetValue = userDbProperty.GetValue(projectDb);
+
+                if (userSourceValue != null &&
+                    !Equals(userSourceValue, new DateTime()) &&
+                    !ReferenceEquals(userSourceValue, "") && !userSourceValue.Equals(userTargetValue))
+                {
+                    userDbProperty.SetValue(projectDb, userSourceValue);
+                }
+            }
+
+            await projectRepository.UpdateProjectAsync(projectDb, cancellationToken);
+            return mapper.Map<ProjectModel>(projectDb);
+        }
+
+        throw new NotPermissionException("You have no access");
     }
 
     public async Task AddEmployeesInProjectAsync(int projectManagerId, int projectId, ICollection<int> employeeModelsIds,
