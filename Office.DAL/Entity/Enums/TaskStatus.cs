88a5540dd67a72f71bffe738ee288fc9e078d16e
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-09-16 14:05:29 +0300

    task entity

---
+++ Office.DAL/Entity/Enums/TaskStatus.cs
@@ -0,0 +1,9 @@
+namespace Office.DAL.Entity.Enums;
+
+public enum TaskStatus
+{
+    New = 0,
+    InProgress,
+    Completed,
+    Cancelled
+}
---
+++ Office.DAL/Entity/TaskEntity.cs
@@ -0,0 +1,20 @@
+using Office.DAL.Entity.Employees;
+
+namespace Office.DAL.Entity;
+
+public class TaskEntity : BaseEntity
+{
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; } = null!;
+
+    public int? ProjectId { get; set; }
+    public Project? Project { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    
+    public string Title { get; set; }  = null!;
+    public string Description { get; set; }  = null!;
+
+    public TaskStatus TaskStatus { get; set; }
+}
