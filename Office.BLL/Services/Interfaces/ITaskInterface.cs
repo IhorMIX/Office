252a717ae808341e4fb8629cdc13aed0fe288784
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-09-18 22:20:36 +0300

    interface

---
+++ Office.BLL/Services/Interfaces/ITaskInterface.cs
@@ -0,0 +1,15 @@
+using Office.BLL.Models;
+
+namespace Office.BLL.Services.Interfaces;
+
+public interface ITaskInterface: IBasicService<TaskEntityModel>
+{
+    Task<TaskEntityModel> CreateTaskAsync(int creatorId,  TaskEntityModel taskEntityModel,
+        CancellationToken cancellationToken);
+
+    Task<TaskEntityModel> UpdateTaskAsync(int managerId, LeaveRequestModel leaveRequestModel,
+        CancellationToken cancellationToken = default);
+
+    Task DeleteTaskAsync(int managerId, int taskId, CancellationToken cancellationToken = default);
+    Task<List<TaskEntityModel>> GetAllAsync(int employeeId, CancellationToken cancellationToken = default);
+}
