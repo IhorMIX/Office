06e35e3fc437b8cb79c9261ad1a163c514bb648e
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-09-24 19:22:50 +0300

    ViewModel

---
+++ Office.Web/Models/ProjectModels/TaskViewModel.cs
@@ -0,0 +1,18 @@
+namespace Office.Web.Models;
+
+public class TaskViewModel
+{
+    public int EmployeeId { get; set; }
+    public EmployeeViewModel Employee { get; set; } = null!;
+
+    public int? ProjectId { get; set; }
+    public ProjectViewModel? Project { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    
+    public string Title { get; set; }  = null!;
+    public string Description { get; set; }  = null!;
+
+    public bool Status { get; set; }
+}
