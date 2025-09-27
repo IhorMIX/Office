967c6f271aa5080ca391e6e48dfa520977960a7b
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-09-27 20:00:26 +0300

    controller

--- Office.Web/Controllers/TaskController.cs
+++ Office.Web/Controllers/TaskController.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Office.BLL.Models;
 using Office.BLL.Services.Interfaces;
+using Office.Web.Extensions;
 using Office.Web.Models;
 
 namespace Office.Web.Controllers;
@@ -16,6 +18,30 @@ public class TaskController(ITaskService taskService, IMapper mapper)
     public async Task<IActionResult> GetById(int taskId, CancellationToken cancellationToken = default)
     {
         var project = await taskService.GetByIdAsync(taskId, cancellationToken);
+        return Ok(mapper.Map<TaskViewModel>(project));
+    }
+    
+    [HttpPost("create-task")]
+    public async Task<IActionResult> CreateTask([FromBody] TaskCreateModel taskCreateModel, CancellationToken cancellationToken = default)
+    {
+        var adminId = User.GetUserId();
+        var result = await taskService.CreateTaskAsync(adminId, mapper.Map<TaskEntityModel>(taskCreateModel),  cancellationToken);
+        return Ok(mapper.Map<TaskViewModel>(result));
+    }
+    
+    [HttpDelete("{taskId:int}")]
+    public async Task<IActionResult> DeleteTask(int taskId, CancellationToken cancellationToken = default)
+    {
+        var managerId = User.GetUserId();
+        await taskService.DeleteTaskAsync(managerId,taskId,cancellationToken);
+        return Ok();
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateTask([FromBody]TaskUpdateModel taskUpdateModel, CancellationToken cancellationToken = default)
+    {
+        var managerId = User.GetUserId();
+        var project= await taskService.UpdateTaskAsync(managerId,mapper.Map<TaskEntityModel>(taskUpdateModel), cancellationToken);
         return Ok(mapper.Map<TaskViewModel>(project));
     }
 }
