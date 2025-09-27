a4261a1c8eab0e338ed53a81d729562201db9d8b
Ігор Колобилін <igor.kolobulin@gmail.com>
2025-09-27 19:59:41 +0300

    model

---
+++ Office.Web/Models/ProjectModels/TaskUpdateModel.cs
@@ -0,0 +1,15 @@
+using TaskStatus = Office.DAL.Entity.Enums.TaskStatus;
+
+namespace Office.Web.Models;
+
+public class TaskUpdateModel
+{
+    public int Id { get; set; }
+    public int? EmployeeId { get; set; }
+    public int? ProjectId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public TaskStatus TaskStatus { get; set; }
+    public string Title { get; set; }  = null!;
+    public string Description { get; set; }  = null!;
+}
